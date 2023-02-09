0abcf2f633151cccf575303e251b6791f8afdb73
dg1223 <alavi1223@hotmail.com>
2023-02-09 18:44:19 -0500

    set onclick events

--- js/events.js
+++ js/events.js
@@ -1,1 +1,27 @@
-console.log('this is a separate JS file');
+// add onclick function
+function makeRed () {
+    document.body.style.backgroundColor = 'red';
+}
+
+function makeGreen () {
+    document.body.style.backgroundColor = 'green';
+}
+
+function makeLightGreen () {
+    document.body.style.backgroundColor = 'lightgreen';
+}
+
+function makeYellowGreen () {
+    document.body.style.backgroundColor = 'yellowgreen';
+}
+
+function makePink () {
+    document.body.style.backgroundColor = 'pink';
+}
+
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+        
+function makeBlue () {
+    document.body.style.backgroundColor = 'blue';
+}  
