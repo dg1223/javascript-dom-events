837de124feee665290b31311e6ed980b7757e116
dg1223 <alavi1223@hotmail.com>
2023-02-09 19:48:44 -0500

    more ways of setting onclick events

--- js/events.js
+++ js/events.js
@@ -1,10 +1,8 @@
 // add onclick function
+
+// option 1
 function makeRed () {
     document.body.style.backgroundColor = 'red';
-}
-
-function makeGreen () {
-    document.body.style.backgroundColor = 'green';
 }
 
 function makeLightGreen () {
@@ -15,13 +13,35 @@ function makeYellowGreen () {
     document.body.style.backgroundColor = 'yellowgreen';
 }
 
-function makePink () {
+// option 2
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;    
+function makeBlue () {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// option 3
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple';            
+}
+
+// option 4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+
+function makePink(){
     document.body.style.backgroundColor = 'pink';
 }
 
-const makeBlueButton = document.getElementById('make-blue');
-makeBlueButton.onclick = makeBlue;
-        
-function makeBlue () {
-    document.body.style.backgroundColor = 'blue';
-}  
+// option 5
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';            
+});
+
+// option 6
+document.getElementById('make-orange').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'orange';
+    
+})
